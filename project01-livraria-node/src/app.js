673f3594ea579be77a2c29ec4b1e53a305eba55d
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-03-03 17:59:05 -0300

    Update app.js

--- project01-livraria-node/src/app.js
+++ project01-livraria-node/src/app.js
@@ -9,6 +9,13 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 routes(app);
 
 export default app
